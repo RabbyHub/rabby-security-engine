3a6d723bb5949316c9a743093d1abe4f395c75ef
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-04-19 23:09:52 +0800

    fix: type error

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,4 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  transform: {
-    "^.+\\.js$": "babel-jest"
-  },
-  transformIgnorePatterns: [
-    "node_modules/(?!(@debank/rabby-api|@debank/common)/)"
-  ]
+  preset: "ts-jest",
 };
