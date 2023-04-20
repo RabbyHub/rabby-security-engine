88338f63449c562d3b26dbb5293bc240125a1b18
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2023-04-20 11:42:30 +0800

    fix: type

--- src/rules.ts
+++ src/rules.ts
@@ -1,122 +1,125 @@
 import { OpenApiService } from "@debank/rabby-api";
-import { caseInsensitiveCompare } from './utils';
+import { caseInsensitiveCompare } from "./utils";
 
 export interface UserData {
   originWhitelist: string[];
   originBlacklist: string[];
 }
 
-interface Context {
+export interface ContextActionData {
   origin?: {
     communityCount: number;
     url: string;
   };
+}
+
+export interface Context extends ContextActionData {
   userData: UserData;
 }
 
 interface NumberValue {
-  min: number | null, // 最小值
-  minIncluded: boolean, // boolean, 最小值是否为小于等于
-  max: number | null, // 最大值，null 代表无限大（小）
-  maxIncluded: boolean // boolean, 最大值是否为大于等于
+  min: number | null; // 最小值
+  minIncluded: boolean; // boolean, 最小值是否为小于等于
+  max: number | null; // 最大值，null 代表无限大（小）
+  maxIncluded: boolean; // boolean, 最大值是否为大于等于
 }
 
 export interface NumberDefine extends NumberValue {
-  type: 'int' | 'float'
+  type: "int" | "float";
 }
 
 export interface BooleanDefine {
-  type: 'boolean'
+  type: "boolean";
 }
 
 export interface EnumDefine {
-  type: 'enum'
-  list: string[]
-  display: Record<string, string>
+  type: "enum";
+  list: string[];
+  display: Record<string, string>;
 }
 
 export enum Level {
-  SAFE = 'safe',
-  WARNING = 'warning',
-  DANGER = 'danger',
-  FORBIDDEN = 'forbidden',
-  ERROR = 'error',
+  SAFE = "safe",
+  WARNING = "warning",
+  DANGER = "danger",
+  FORBIDDEN = "forbidden",
+  ERROR = "error",
 }
 
 export type Threshold = {
-  [key in Level]?: NumberValue | boolean | string[]
+  [key in Level]?: NumberValue | boolean | string[];
 };
 
 export interface RuleConfig {
-  id: string // 规则 id
-  enable: boolean // 是否启用
-  valueDescription: string // value 描述
-  valueDefine: NumberDefine | BooleanDefine | EnumDefine // value 定义
-  defaultThreshold: Threshold // 默认阈值
-  customThreshold: Threshold // 用户自定义阈值
-  requires: string[] // 规则运行依赖的主属性，需要全部有值才执行该规则
-  getValue(ctx: Context, apiService: OpenApiService): Promise<any> // 取值函数，从引擎外部获取数据
+  id: string; // 规则 id
+  enable: boolean; // 是否启用
+  valueDescription: string; // value 描述
+  valueDefine: NumberDefine | BooleanDefine | EnumDefine; // value 定义
+  defaultThreshold: Threshold; // 默认阈值
+  customThreshold: Threshold; // 用户自定义阈值
+  requires: string[]; // 规则运行依赖的主属性，需要全部有值才执行该规则
+  getValue(ctx: Context, apiService: OpenApiService): Promise<any>; // 取值函数，从引擎外部获取数据
 }
 
 export const defaultRules: RuleConfig[] = [
   {
     // Origin 被 Rabby 标记为欺诈网址
-    id: '1001',
+    id: "1001",
     enable: true,
-    valueDescription: '',
+    valueDescription: "",
     valueDefine: {
-      type: 'boolean'
+      type: "boolean",
     },
     defaultThreshold: {
       danger: true,
     },
     customThreshold: {},
-    requires: ['origin'],
+    requires: ["origin"],
     async getValue(ctx, apiService) {
       // TODO
-    }
+    },
   },
   {
     // Origin 被 MetaMask 标记为欺诈网址
-    id: '1002',
+    id: "1002",
     enable: true,
-    valueDescription: '',
+    valueDescription: "",
     valueDefine: {
-      type: 'boolean'
+      type: "boolean",
     },
     defaultThreshold: {
       danger: true,
     },
     customThreshold: {},
-    requires: ['origin'],
+    requires: ["origin"],
     async getValue(ctx, apiService) {
       // TODO
-    }
+    },
   },
   {
     // Origin 被 ScamSniffer 标记为欺诈网址
-    id: '1003',
+    id: "1003",
     enable: true,
-    valueDescription: '',
+    valueDescription: "",
     valueDefine: {
-      type: 'boolean'
+      type: "boolean",
     },
     defaultThreshold: {
       danger: true,
     },
     customThreshold: {},
-    requires: ['origin'],
+    requires: ["origin"],
     async getValue(ctx, apiService) {
       // TODO
-    }
+    },
   },
   {
     // Origin 被知名社区平台收录数
-    id: '1004',
+    id: "1004",
     enable: true,
-    valueDescription: '',
+    valueDescription: "",
     valueDefine: {
-      type: 'int',
+      type: "int",
       min: 0,
       minIncluded: true,
       max: 100,
@@ -137,68 +140,72 @@ export const defaultRules: RuleConfig[] = [
       },
     },
     customThreshold: {},
-    requires: ['origin'],
+    requires: ["origin"],
     async getValue(ctx, apiService) {
       // TODO
-    }
+    },
   },
   {
     // Origin 使用人数
-    id: '1005',
+    id: "1005",
     enable: true,
-    valueDescription: '',
+    valueDescription: "",
     valueDefine: {
-      type: 'enum',
-      list: ['very_low', 'low', 'average', 'high'],
+      type: "enum",
+      list: ["very_low", "low", "average", "high"],
       display: {
-        'very_low': 'Very Low',
-        low: 'Low',
-        average: 'Average',
-        high: 'High',
-      }
+        very_low: "Very Low",
+        low: "Low",
+        average: "Average",
+        high: "High",
+      },
     },
     defaultThreshold: {
-      danger: ['very_low'],
-      warning: ['low'],
+      danger: ["very_low"],
+      warning: ["low"],
     },
     customThreshold: {},
-    requires: ['origin'],
+    requires: ["origin"],
     async getValue(ctx, apiService) {
       // TODO
-    }
+    },
   },
   {
     // Origin 在用户黑名单中
-    id: '1006',
+    id: "1006",
     enable: true,
-    valueDescription: '',
+    valueDescription: "",
     valueDefine: {
-      type: 'boolean'
+      type: "boolean",
     },
     defaultThreshold: {
       forbidden: true,
     },
     customThreshold: {},
-    requires: ['origin'],
+    requires: ["origin"],
     async getValue(ctx) {
-      return ctx.userData.originBlacklist.some(item => caseInsensitiveCompare(item, ctx.origin!.url))
-    }
+      return ctx.userData.originBlacklist.some((item) =>
+        caseInsensitiveCompare(item, ctx.origin!.url)
+      );
+    },
   },
   {
     // Origin 在用户白名单中
-    id: '1007',
+    id: "1007",
     enable: true,
-    valueDescription: '',
+    valueDescription: "",
     valueDefine: {
-      type: 'boolean'
+      type: "boolean",
     },
     defaultThreshold: {
       safe: true,
     },
     customThreshold: {},
-    requires: ['origin'],
+    requires: ["origin"],
     async getValue(ctx) {
-      return ctx.userData.originBlacklist.some(item => caseInsensitiveCompare(item, ctx.origin!.url))
-    }
+      return ctx.userData.originBlacklist.some((item) =>
+        caseInsensitiveCompare(item, ctx.origin!.url)
+      );
+    },
   },
 ];
