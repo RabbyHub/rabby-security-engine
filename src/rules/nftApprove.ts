8659f76e13fb5d7fe2daedf16972fafdadc5201e
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2024-05-22 15:29:58 +0800

    feat: remove nft approve trust value rule

--- src/rules/nftApprove.ts
+++ src/rules/nftApprove.ts
@@ -19,40 +19,40 @@ const rules: RuleConfig[] = [
       return isEOA;
     },
   },
-  {
-    id: "1044",
-    enable: true,
-    valueDescription: "Trust value",
-    valueTooltip:
-      "Trust value refers to the top NFT approved and exposed to this contract. A low trust value indicates either risk or inactivity for 180 days.",
-    valueDefine: {
-      type: "int",
-      min: 0,
-      minIncluded: true,
-      max: null,
-      maxIncluded: false,
-    },
-    defaultThreshold: {
-      danger: {
-        min: 0,
-        minIncluded: true,
-        max: 10000,
-        maxIncluded: true,
-      },
-      warning: {
-        min: 10000,
-        minIncluded: false,
-        max: 100000,
-        maxIncluded: true,
-      },
-    },
-    customThreshold: {},
-    requires: ["nftApprove"],
-    async getValue(ctx) {
-      const data = ctx.nftApprove!;
-      return data.riskExposure;
-    },
-  },
+  // {
+  //   id: "1044",
+  //   enable: true,
+  //   valueDescription: "Trust value",
+  //   valueTooltip:
+  //     "Trust value refers to the top NFT approved and exposed to this contract. A low trust value indicates either risk or inactivity for 180 days.",
+  //   valueDefine: {
+  //     type: "int",
+  //     min: 0,
+  //     minIncluded: true,
+  //     max: null,
+  //     maxIncluded: false,
+  //   },
+  //   defaultThreshold: {
+  //     danger: {
+  //       min: 0,
+  //       minIncluded: true,
+  //       max: 10000,
+  //       maxIncluded: true,
+  //     },
+  //     warning: {
+  //       min: 10000,
+  //       minIncluded: false,
+  //       max: 100000,
+  //       maxIncluded: true,
+  //     },
+  //   },
+  //   customThreshold: {},
+  //   requires: ["nftApprove"],
+  //   async getValue(ctx) {
+  //     const data = ctx.nftApprove!;
+  //     return data.riskExposure;
+  //   },
+  // },
   {
     id: "1045",
     enable: true,
